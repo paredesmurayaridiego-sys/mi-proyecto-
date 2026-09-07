62f988caf2cccc4f2b86589b1beb1eff791dcd65
Diego <paredesmurayaridiego.@gmail.com>
2026-09-07 00:26:07 -0500

    Actualizar ingreso de notas

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hola,mundo!")
+print("Hola, mundo!")
+
+print("--- INGRESO DE NOTAS DEL CURSO ---")
+
+x = 0
+
+while x == 0:
+    for i in range(1, 4):
+        print("Ingrese nota " + str(i) + ": ")
+        nota = int(input())
+
+    estudiante = input("¿Necesita ingresar nuevo estudiante? (s/n): ")
+
+    if estudiante == 's':
+        x = 0
+    else:
+        x = 1
